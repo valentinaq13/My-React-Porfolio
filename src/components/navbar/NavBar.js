3216d4c46aad4ec9c3dc911ea9c5fa26dd1f4c6b
valentinaq13 <emeritavalentina@gmail.com>
2022-04-08 13:45:46 -0300

    improving contact info

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -4,11 +4,15 @@ import "./NavBar.css";
 
 const NavBar = ({ isScrolling }) => {
   const toTheTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }
+  const toTheBottom = () => {
     window.scrollTo(0, document.body.scrollHeight );
   }
   return (
     <nav className={`navbar ${isScrolling > 20 ? "scrolling" : null}`}>
-      <div className='navbar-logo' onClick={toTheTop}>Contact</div>
+      <div className='navbar-logo' onClick={toTheTop}>VQ</div>
+      <div className='navbar-logo' onClick={toTheBottom}>Contact me</div>
 
     </nav>
   )
